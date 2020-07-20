a5fcbad0a23e14f40d1475f2b69c616893b75164
IAmDonut276 <gracelimjingyao@gmail.com>
2020-07-20 20:26:34 +0800

    Read Data

--- app/src/main/java/com/example/onegmall/DonutCurrencyCheck.java
+++ app/src/main/java/com/example/onegmall/DonutCurrencyCheck.java
@@ -8,56 +8,72 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Currency;
+
 public class DonutCurrencyCheck extends AppCompatActivity {
-Button btn1;
-//Button btn2;
-//String TAG ="g";
-FirebaseDatabase database=FirebaseDatabase.getInstance();
+
+    TextView txt;
+    DatabaseReference myRef;
+    String TAG ="g";
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    FirebaseAuth mAuth=FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener mAuthListener;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donut_currency_check);
 
-        btn1= findViewById(R.id.button5);
-        //btn2= findViewById(R.id.button6);
+        myRef= database.getReference();
+        FirebaseUser user =mAuth.getCurrentUser();
+        userID= user.getUid();
+        txt =findViewById(R.id.textView5);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                DatabaseReference myRef = database.getReference("message");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
+            }
 
-                myRef.setValue("Hello World");
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+    }
 
-        /*btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatabaseReference myRef=database.getReference("message");
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        String value =snapshot.getValue(String.class);
-                        Log.d(TAG,"Value is:"+ value);
-                    }
+    private void showData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot ds : dataSnapshot.getChildren()){
+            UserData uData=new UserData();
+            uData.setDonut_Cafe(ds.child(userID).getValue(UserData.class).getDonut_Cafe());
+            uData.setLifeAfter(ds.child(userID).getValue(UserData.class).getLifeAfter());
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.w(TAG,"Failed to read value.",error.toException());
-                    }
-                });
-            }
-        });*/
+            //display all the items
+            Log.d(TAG, "showData: Donut Cafe: "+ uData.getDonut_Cafe());
+            Log.d(TAG,"showData: LifeAfter: "+ uData.getLifeAfter());
+
+
+            txt.setText();
+        }
     }
 
     public void totopupdonut(View view) {
---
+++ app/src/main/java/com/example/onegmall/UserData.java
@@ -0,0 +1,26 @@
+package com.example.onegmall;
+
+public class UserData {
+ String Donut_Cafe;
+ String LifeAfter;
+
+ public UserData(){
+
+ }
+
+    public String getDonut_Cafe() {
+        return Donut_Cafe;
+    }
+
+    public void setDonut_Cafe(String donut_Cafe) {
+        Donut_Cafe = donut_Cafe;
+    }
+
+    public String getLifeAfter() {
+        return LifeAfter;
+    }
+
+    public void setLifeAfter(String lifeAfter) {
+        LifeAfter = lifeAfter;
+    }
+}
