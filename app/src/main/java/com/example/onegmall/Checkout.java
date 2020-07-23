7083cc37616b453ccb2d9aef3ddf3f40dabba5e0
IAmDonut276 <gracelimjingyao@gmail.com>
2020-07-23 11:04:43 +0800

    Database complete

--- app/src/main/java/com/example/onegmall/Checkout.java
+++ app/src/main/java/com/example/onegmall/Checkout.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -15,14 +16,22 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Checkout extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView tv;
-    String st;
+    String st,userID;
     Button btn;
+    DatabaseReference myRef;
+    String TAG ="g";
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    FirebaseAuth mAuth=FirebaseAuth.getInstance();
+    Long c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,9 @@ public class Checkout extends AppCompatActivity implements AdapterView.OnItemSel
         btn=findViewById(R.id.button4);
         st=getIntent().getExtras().getString("Id");
         tv.setText(st);
+        myRef= database.getReference();
+        FirebaseUser user =mAuth.getCurrentUser();
+        userID= user.getUid();
 
         Spinner spinner =findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
@@ -41,14 +53,36 @@ public class Checkout extends AppCompatActivity implements AdapterView.OnItemSel
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Checkout.this, Menu.class);
-                startActivity(intent);
-                Toast.makeText(Checkout.this, "Bought Complete", Toast.LENGTH_SHORT).show();
-            }
+                    Long C = new Long(c+1500);
+                    myRef.child(userID).child("currency").setValue(C);
+                    Intent intent = new Intent(Checkout.this, Menu.class);
+                    startActivity(intent);
+                    Toast.makeText(Checkout.this, "Bought Complete", Toast.LENGTH_SHORT).show();
+                }
         });
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+        c=(Long)dataSnapshot.child(userID).child("currency").getValue();
     }
 
     @Override
